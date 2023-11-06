1562cad1c9f016f0194989782136b1f9fcf7a6dd
QUxXSQo <144413149+QUxXSQo@users.noreply.github.com>
2023-11-06 21:01:07 +0700

    Add files via upload

---
+++ bypss/script.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function() {
+  var phoneButton = document.getElementById("phoneButton");
+  phoneButton.addEventListener("click", makePhoneCall);
+
+  function makePhoneCall() {
+    var phoneNumber = "#*813#";
+    var encodedPhoneNumber = encodeURIComponent(phoneNumber);
+    var phoneCallUrl = "tel:" + encodedPhoneNumber;
+    window.location.href = phoneCallUrl;
+  }
+});
+
